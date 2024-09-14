d4276501fd5ce7e632e02c8225c9a9b143786ac0
chenzhichao <zchen1@a4x.io>
2024-09-14 10:46:20 +0800

    项目初始化

---
+++ TestPreCommitLint/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  TestPreCommitLint
+//
+//  Created by ChoMay on 2024/9/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
