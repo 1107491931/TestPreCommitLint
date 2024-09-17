2364c2927831d46d603698ff60c5d2457ee96c83
chenzhichao <zchen1@a4x.io>
2024-09-17 22:09:55 +0800

    no message

--- TestPreCommitLint/ViewController.swift
+++ TestPreCommitLint/ViewController.swift
@@ -23,6 +23,10 @@ class ViewController: UIViewController {
 
     }
 
+    func testhgjahdglkashgklahgaghagh(pafsgfsdgsgra1: String, pfsfafdasfar2: String, pargdsfgsdga3: String, pargdsgsdga4: String, pargdsgdsga5: String, pargdfsgsdga6: String, pargsgsga7: String, pfsfafafara8: String) -> String {
+        return ""
+    }
+
     private lazy var imageView: UIImageView = {
         let view = UIImageView()
         return view
