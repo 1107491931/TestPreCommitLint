0aaa963411fe333a5131f538add68f309b673643
chenzhichao <zchen1@a4x.io>
2024-09-18 22:27:51 +0800

    test

--- TestPreCommitLint/ViewController.swift
+++ TestPreCommitLint/ViewController.swift
@@ -11,7 +11,7 @@ typealias VoidCallBack = (String) -> Void
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print(CGRectZero)
     }
 
     func test(name: String, callBack: VoidCallBack) {
