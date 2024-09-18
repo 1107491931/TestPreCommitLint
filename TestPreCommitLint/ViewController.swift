ee2d1da556fbc655d470f574be2eb5a8a0fd3a29
chenzhichao <zchen1@a4x.io>
2024-09-18 22:45:23 +0800

    test

--- TestPreCommitLint/ViewController.swift
+++ TestPreCommitLint/ViewController.swift
@@ -20,7 +20,9 @@ class ViewController: UIViewController {
     }
     
     // MARK: 11111
-    func test(name: String, name1: String, name2: String) {
+    func test(
+        name: String, name1: String, 
+              name2: String) {
         let x11111 = [
             1,
             2
