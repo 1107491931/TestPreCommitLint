6e67017fc1b602b43d37cb4daf2bfba581a63cb1
chenzhichao <zchen1@a4x.io>
2024-09-18 19:09:50 +0800

    no message

--- TestPreCommitLint/ViewController.swift
+++ TestPreCommitLint/ViewController.swift
@@ -12,19 +12,13 @@ typealias VoidCallBack = (String) -> Void
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        let list = [0, 1]
+        if list.isEmpty {
+            return
+        }
     }
 
     func test(name: String, callBack: VoidCallBack) {
 
     }
 }
-
-/*
- 安装插件：brew install pre-commit
- 项目目录下执行：pre-commit install  // 之后commit时会执行.git/hooks/pre-commit中的脚本代码
- 
- // 生成yaml文件：pre-commit sample-config > .pre-commit-config.yaml
- // 手动执行检测： pre-commit run --all-files
- // 此后提交代码就会自动检测，如果想跳过检测： git commit --no-verify -m "xxx"
- 
- */
